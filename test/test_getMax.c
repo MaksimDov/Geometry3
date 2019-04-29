2fc7c5324d8d842e8978542d759283441f425ad3
MaksimDov <maksimdovydenko@mail.ru>
2019-04-29 22:05:43 +0700

    Fixe clang-format for travis 3

--- test/test_getMax.c
+++ test/test_getMax.c
@@ -1,14 +1,10 @@
 #include "geometry.h"
 
-double getMax(double x1, double x2)
-{
-    return x1 > x2 ? x1 : x2;
-}
-TEST_CASE("GETMAX", "[MAX]")
-{
-    REQUIRE(getMax(1, 3) == 3);
-    REQUIRE(getMax(5, 10) == 10);
-    REQUIRE(getMax(-100, 100) == 100);
-    REQUIRE(getMax(-36.6, 0) == 0);
-    cout << "Test getMax" << endl;
+double getMax(double x1, double x2) { return x1 > x2 ? x1 : x2; }
+TEST_CASE("GETMAX", "[MAX]") {
+  REQUIRE(getMax(1, 3) == 3);
+  REQUIRE(getMax(5, 10) == 10);
+  REQUIRE(getMax(-100, 100) == 100);
+  REQUIRE(getMax(-36.6, 0) == 0);
+  cout << "Test getMax" << endl;
 }
